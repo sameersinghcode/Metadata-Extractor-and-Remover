bd29936018cad5621cdf05018ea7b2e0e555c744
0x0elliot <wolfwithahat@protonmail.com>
2022-06-16 05:38:41 +0530

    To show

---
+++ main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.*;
+
+class main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter image absolute path: ");
+        
+        String abspath = s.nextLine();
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"python3", "main.py", abspath};
+        
+        try {
+            Process proc = rt.exec(commands);
+            BufferedReader stdInput = new BufferedReader(new 
+            InputStreamReader(proc.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new 
+            InputStreamReader(proc.getErrorStream()));
+
+            String j = null;
+            while ((j = stdInput.readLine()) != null) {
+                System.out.println(j);
+            }
+            while ((j = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("error!");
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from PIL import Image
+
+image_abs_path = sys.argv[1]
+
+if not os.path.exists(image_abs_path):
+    print("Image doesn't exist!")
+
+
+image = Image.open(image_abs_path)
+
+# next 3 lines strip exif
+data = list(image.getdata())
+image_without_exif = Image.new(image.mode, image.size)
+image_without_exif.putdata(data)
+
+os.remove(image_abs_path)
+
+image_without_exif.save(image_abs_path)
+print("Successfully overwritten file!")
